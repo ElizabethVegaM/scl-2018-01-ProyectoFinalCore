1c43f2ad9583a5bca3c9e42e19efef2c50ec37f9
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-07-28 20:04:10 -0400

    se crean visitantes en firebase con datos básicos, trabajando

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -7,9 +7,8 @@ jQuery(document).ready(function($) {
   $('#visitorRut').rut();
 });
 
-
+// Traer archivo de empleados
 let employees = {};
-// Taer archivo de empleados
 const dataEmployee = () => {
   fetch('../../../data/employees.json')
     .then(response => response.json())
@@ -18,3 +17,23 @@ const dataEmployee = () => {
       console.log(employees);      
     });
 };
+
+// registrar usuarios
+registerUser.addEventListener('click', () => {
+  const newUserKey = firebase.database().ref().child('visitors').push().key;
+  firebase.database().ref(`visitors/${newUserKey}`).set({
+    name: visitorName.value,
+    rut: visitorRut.value,
+    email: visitorEmail.value,
+    company: inputCompany.value,
+    employeeToVisit: inputEmployee.value,
+    reason: inputReason.value,
+    photo: 'hay que poner algo mientras'
+  });
+});
+
+firebase.database().ref('visitors')
+  .limitToLast(10)
+  .on('child_added', (newVisitor) => {
+    console.log(newVisitor);  
+  });
